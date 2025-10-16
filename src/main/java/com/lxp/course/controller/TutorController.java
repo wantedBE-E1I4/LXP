80d3bd3e63931d0d4931ba85fffe1bc2d077e085
Juyeong Moon <whoor2828@gmail.com>
2025-10-16 15:17:42 +0900

    feat(controller): TutorController.process()에 강의 관리 진입 로직 추가

--- src/main/java/com/lxp/course/controller/TutorController.java
+++ src/main/java/com/lxp/course/controller/TutorController.java
@@ -1,10 +1,14 @@
 package com.lxp.course.controller;
 
+import com.lxp.course.LectureDAO;
+import com.lxp.course.LectureService;
 import com.lxp.user.User;
+import com.lxp.view.tutor.LectureManagementView;
 
 // 강사 메뉴 및 기능 흐름 제어
 public class TutorController {
     private final User tutor;
+    private LectureManagementView lectureManagementView = new LectureManagementView();
 
     public TutorController(User tutor) {
         this.tutor = tutor;
@@ -14,5 +18,6 @@ public class TutorController {
         // TODO: LearnerView.showMenu();
         // TODO: 사용자 입력값에 따라 수강 목록 조회 / 수강 신청 등 분기 처리
         System.out.println("강사 메뉴 출력");
+        lectureManagementView.show();
     }
 }
