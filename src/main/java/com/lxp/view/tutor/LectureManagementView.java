8ab6f6fb656039fce55b6dfc1ec7f71165a0e6ca
Juyeong Moon <whoor2828@gmail.com>
2025-10-16 14:34:39 +0900

    feat: 강의 관리 메뉴 진입점 추가 - LectureManagementView 생성 및 의존성 주입 - 강좌 선택 후 강의 목록/관리 진입 연결

--- src/main/java/com/lxp/view/tutor/LectureManagementView.java
+++ src/main/java/com/lxp/view/tutor/LectureManagementView.java
@@ -1,5 +1,26 @@
 package com.lxp.view.tutor;
 
+import com.lxp.common.Displayable;
+import com.lxp.course.Lecture;
+import com.lxp.course.LectureDAO;
+import com.lxp.course.LectureService;
+
+import java.util.List;
+import java.util.Scanner;
+
 // 강의 관리 메뉴
-public class LectureManagementView {
+public class LectureManagementView implements Displayable {
+    Scanner scanner = new Scanner(System.in);
+    LectureDAO lectureDAO = new LectureDAO();
+    LectureService lectureService = new LectureService(lectureDAO);
+
+    @Override
+    public void show() {
+        // 1) 강좌-관리 목록
+        lectureService.courseManage();
+        int courseId = scanner.nextInt();
+
+        // 2) 강의 관리
+        List<Lecture> lectures = lectureService.lectureManage(courseId);
+    }
 }
