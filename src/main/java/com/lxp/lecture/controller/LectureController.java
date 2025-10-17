ba5633c9f53b8ca8a263a82a2103a9a912f987c6
Juyeong Moon <whoor2828@gmail.com>
2025-10-17 17:52:42 +0900

    feat: merge 후 적용

--- src/main/java/com/lxp/lecture/controller/LectureController.java
+++ src/main/java/com/lxp/lecture/controller/LectureController.java
@@ -19,57 +19,17 @@ public class LectureController {
     private CourseService courseService;
     private EnrollmentService enrollmentService;
     public LectureController(LectureService lectureService, EnrollmentService enrollmentService, CourseService courseService) {
-
-    public LectureController(LectureService lectureService) {
         this.lectureService = lectureService;
         this.enrollmentService = enrollmentService;
         this.courseService = courseService;
     }
-    public void showLecturesByCourse(Scanner scanner, int userId) {
-        System.out.println("\n--- [내 강좌 리스트] ---");
-        // 강좌 리스트 출력 TODO : 재웅님이 작업하고 있는 부분
-        List<EnrollmentData> enrollments = enrollmentService.getEnrollmentsByUser(userId);
-        for (EnrollmentData enrollment : enrollments) {
-            System.out.println(enrollment.courseId() + ". " + enrollment.courseTitle() + " - " + enrollment.tutorName());
-        }
-        System.out.println();
-        System.out.println("조회할 강좌의 번호를 입력해주세요.");
-        System.out.println("0. 종료"); // TODO : 종료를 해야할지 App main으로 돌아가야 할지 고민 !
-        System.out.print(">> ");
-        String courseChoice = scanner.nextLine();
-        switch (courseChoice) {
-            case "0" :
-                System.out.println("프로그램을 종료합니다.");
-                scanner.close();
-                return;
-            default:
-                lectureCourse(scanner, courseChoice);
-                break;
-        }
-    }
-
-    private void lectureCourse(Scanner scanner, String courseId) {
-        int convertIntCourseId = Integer.parseInt(courseId);
-
-        // 강좌 제목
-        CourseDetail course = courseService.getCourseDetail(convertIntCourseId);
-        System.out.println("--- " + course.title() + " ---");
-
-        // 강의 리스트
-        // NOTE : Service layer에 값을 넘겨줄 때는 단순 데이터를 넘겨줘야 합니다.
-        // TODO : LectureService는 주영님이 만든 메서드로 수행.
-        List<LecturesByCourse> lectures = lectureService.getLecturesByCourse(convertIntCourseId);
-
-        for (LecturesByCourse lecture : lectures) {
-            System.out.println(lecture.orderNo() + ". " +  lecture.title());
-        }
 
 
 
     //강좌별 강의조회
     public void showLecturesByCourse(int courseId) {
-        String title = courseService.getCourseTitle();
-        System.out.println("== " + title + " ==");
+        //String title = courseService.getCourseTitle();
+        //System.out.println("== " + title + " ==");
         List<Lecture> lectures = lectureService.getLectureList(courseId);
         if (lectures.isEmpty()) {
             System.out.println("강의가 없습니다.");
