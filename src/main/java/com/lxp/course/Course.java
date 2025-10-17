b1547f31e95332033ec464fadab42a9893c832f3
Juyeong Moon <whoor2828@gmail.com>
2025-10-17 17:46:18 +0900

    feat: merge 후 적용

--- src/main/java/com/lxp/course/Course.java
+++ src/main/java/com/lxp/course/Course.java
@@ -1,9 +1,5 @@
 package com.lxp.course;
 
-import com.lxp.lecture.Lecture;
-
-import java.util.Date;
-
 // 강좌 엔티티
 public class Course {
     private int courseId;
@@ -22,53 +18,4 @@ public class Course {
     public static Course createCourse(int tutorId, String title) {
         return new Course(tutorId, title);
     }
-
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
-
-    public int getTutorId() {
-        return tutorId;
-    }
-
-    public void setTutorId(int tutorId) {
-        this.tutorId = tutorId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getDelFlg() {
-        return delFlg;
-    }
-
-    public void setDelFlg(int delFlg) {
-        this.delFlg = delFlg;
-    }
-
 }
--- src/main/java/com/lxp/course/service/CourseService.java
+++ src/main/java/com/lxp/course/service/CourseService.java
@@ -1,13 +1,8 @@
 package com.lxp.course.service;
 
-import com.lxp.course.Course;
 import com.lxp.course.EnrollmentDAO;
 import com.lxp.course.dao.CourseDAO;
-import com.lxp.course.dto.CourseData;
-import com.lxp.course.dto.CourseDetail;
 import com.lxp.lecture.dao.LectureDAO;
-
-import java.util.List;
 
 // 강좌 관련 로직
 public class CourseService {
@@ -21,14 +16,5 @@ public class CourseService {
         this.enrollmentDAO = enrollmentDAO;
     }
 
-    public CourseDetail getCourseDetail(int courseId) {
-        CourseDetail findedCourse =  courseDAO.findById(courseId);
 
-        return findedCourse;
-    }
-
-    public List<CourseData> getCoursesByUser(int userId) {
-        List<CourseData> findedCourses = courseDAO.findByUserId(userId);
-        return findedCourses;
-    }
 }
