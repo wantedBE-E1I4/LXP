33623ab76b42ee4a4c926ce7e298b7c9dab3c1df
JSeHoone <beebea545@gmail.com>
2025-10-17 14:21:33 +0900

    feat : course, enrollment, lecture Service 추가

--- src/main/java/com/lxp/course/EnrollmentService.java
+++ src/main/java/com/lxp/course/EnrollmentService.java
@@ -1,10 +1,20 @@
 package com.lxp.course;
 
+import com.lxp.course.dto.CourseData;
+import com.lxp.course.dto.EnrollmentData;
+
+import java.util.List;
+
 // 수강 신청/취소 로직
 public class EnrollmentService {
     private EnrollmentDAO enrollmentDAO;
 
     public EnrollmentService(EnrollmentDAO enrollmentDAO) {
         this.enrollmentDAO = enrollmentDAO;
+    }
+
+    public List<EnrollmentData> getEnrollmentsByUser(int userId) {
+        List<EnrollmentData> response = enrollmentDAO.findByUserId(userId);
+        return response;
     }
 }
--- src/main/java/com/lxp/lecture/service/LectureService.java
+++ src/main/java/com/lxp/lecture/service/LectureService.java
@@ -1,11 +1,21 @@
 package com.lxp.lecture.service;
 
+import com.lxp.course.Course;
 import com.lxp.lecture.dao.LectureDAO;
+import com.lxp.lecture.dto.LecturesByCourse;
+
+import java.util.List;
 
 public class LectureService {
     private final LectureDAO lectureDAO;
 
     public LectureService(LectureDAO lectureDAO) {
         this.lectureDAO = lectureDAO;
+    }
+
+    public List<LecturesByCourse> getLecturesByCourse(int courseId) {
+
+        List<LecturesByCourse> response = lectureDAO.findByCourseId(courseId);
+        return response;
     }
 }
