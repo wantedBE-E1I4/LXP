dc3a8e6e8438683f024432ebd85c97d1d4c19a8e
Juyeong Moon <whoor2828@gmail.com>
2025-10-17 16:02:45 +0900

    style: 불필요한 주석 제거

--- src/main/java/com/lxp/lecture/controller/LectureController.java
+++ src/main/java/com/lxp/lecture/controller/LectureController.java
@@ -1,11 +1,8 @@
 package com.lxp.lecture.controller;
 
-import com.lxp.course.service.CourseService;
 import com.lxp.lecture.Lecture;
-import com.lxp.lecture.dao.LectureDAO;
 import com.lxp.lecture.service.LectureService;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,8 +15,8 @@ public class LectureController {
 
     //강좌별 강의조회
     public void showLecturesByCourse(int courseId) {
-        //String title = courseService.getCourseTitle(); //컨트롤러에서 CourseService와 LectureService를 둘 다 호출해 조립하는 게 정석
-        //System.out.println("== " + title + " ==");
+        String title = courseService.getCourseTitle();
+        System.out.println("== " + title + " ==");
         List<Lecture> lectures = lectureService.getLectureList(courseId);
         if (lectures.isEmpty()) {
             System.out.println("강의가 없습니다.");
