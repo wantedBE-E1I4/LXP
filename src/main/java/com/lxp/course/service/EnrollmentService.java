5f1da0da46ced69bb93b122044d85aa4d86da686
JSeHoone <beebea545@gmail.com>
2025-10-18 20:30:01 +0900

    feat : 수강중인 강의 관련 메서드 및 생성 의존성 추가
    - getEnrollmentLectures : 수강중인 강좌의 강의 출력 메서드
    - takeEnrollmentLecture : 수강 중인 강좌의 강의 상태값 변경 메서드
    - lectureProcessDAO 의존성 추가

--- src/main/java/com/lxp/course/service/EnrollmentService.java
+++ src/main/java/com/lxp/course/service/EnrollmentService.java
@@ -1,8 +1,9 @@
 package com.lxp.course.service;
 
 import com.lxp.course.Enrollment;
+import com.lxp.course.dao.LectureProcessDAO;
+import com.lxp.course.dto.EnrollmentLecture;
 import com.lxp.course.dao.EnrollmentDAO;
-import com.lxp.course.dto.EnrollmentData;
 import com.lxp.course.dto.MyEnrollmentCourseInfo;
 
 import java.util.List;
@@ -10,9 +11,11 @@ import java.util.List;
 // 수강 신청/취소 로직
 public class EnrollmentService {
     private EnrollmentDAO enrollmentDAO;
+    private LectureProcessDAO lectureProcessDAO;
 
-    public EnrollmentService(EnrollmentDAO enrollmentDAO) {
+    public EnrollmentService(EnrollmentDAO enrollmentDAO, LectureProcessDAO lectureProcessDAO) {
         this.enrollmentDAO = enrollmentDAO;
+        this.lectureProcessDAO = lectureProcessDAO;
     }
 
     public List<Enrollment> getEnrollmentsByUser(int userId) {
@@ -24,5 +27,19 @@ public class EnrollmentService {
     }
     public List<MyEnrollmentCourseInfo> getMyEnrollments(int userId) {
         return enrollmentDAO.findMyCourseForLearner(userId);
+    }
+
+    /**
+     *  수강중인 강좌의 강의 출력 메서드
+    * */
+    public List<EnrollmentLecture> getEnrollmentLectures(int enrollmentCourseId) {
+        return lectureProcessDAO.findByEnrollmentId(enrollmentCourseId);
+    }
+
+    /**
+     * 수강 중인 강좌의 강의 상태값 변경 메서드
+    * */
+    public int takeEnrollmentLecture(int courseId, int lectureId) {
+        return lectureProcessDAO.setEnrollmentLectureCompleted(courseId, lectureId);
     }
 }
